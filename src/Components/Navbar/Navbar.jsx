24b7613d8799f07d7ef07b0154d3e17796cc0c3d
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2021-10-06 10:33:03 -0300

    Se arregla ruta para ingresar a las categorías

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,8 +8,8 @@ export default function Navbar() {
    <div className = "nav-container">
      <div className ='nav-btn-container'>
       <Link to="/e-commerceCavagnaro"><Button className = 'btn-nav' text = 'Inicio'/></Link>
-      <Link to="/mujer"><Button className = 'btn-nav' text = 'Mujer'/></Link>
-      <Link to="/hombre"><Button className = 'btn-nav' text = 'Hombre'/></Link>
+      <Link to="/Mujer/"><Button className = 'btn-nav' text = 'Mujer'/></Link>
+      <Link to="/Hombre/"><Button className = 'btn-nav' text = 'Hombre'/></Link>
      </div>
      <Link to="/cart"><CartWidget className = 'cart-widget'/></Link>
    </div>
